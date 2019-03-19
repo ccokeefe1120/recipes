00f78f84fbc321d7631a0875bc988a4d92e13d63
Carly <ccokeefe1120@gmail.com>
2019-03-19 09:56:18 -0400

    Added new endpoint to APIClient

--- Recipes/Services/APIClient.swift
+++ Recipes/Services/APIClient.swift
@@ -15,7 +15,7 @@ class APIClient {
     static func getLatestRecipes(completion: @escaping ([Recipe]) -> Void) {
         let url = "https://www.themealdb.com/api/json/v1/1/latest.php"
         
-        Alamofire.request(url).responseJSON { response in // 2
+        Alamofire.request(url).responseJSON { response in
             let jsonData = response.data
             
             do{
@@ -27,6 +27,28 @@ class APIClient {
                 
                 // grab the array of recipes
                 completion(meals.meals)
+                
+            } catch let err {
+                print(err)
+            }
+        }
+    }
+    
+    static func getRecipeCategories(completion: @escaping ([String]) -> Void) {
+        let url = "https://www.themealdb.com/api/json/v1/1/categories.php"
+        
+        Alamofire.request(url).responseJSON { response in
+            let jsonData = response.data
+            
+            do{
+                // created the json decoder
+                let decoder = JSONDecoder()
+                
+                // decode Meals object
+                let categories = try decoder.decode([String].self, from: jsonData!)
+                
+                // grab the array of recipes
+                completion(categories)
                 
             } catch let err {
                 print(err)
